a7a7a067e9d2ec14aca15aa5a9f185e46ceb8dd0
Putra2203 <erdin22xii@gmail.com>
2025-01-12 22:48:54 +0700

    start setting for deploy

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./storage-crowdsourcing-firebase-adminsdk-scnap-89dfeba7c9.json");
+const serviceAccount = require("./storage-crowdsourcing-firebase-adminsdk-scnap-ed2e86ddcd.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
